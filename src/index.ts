80e268a805064140eb0dc241c11ca07bbbb9c845
harukee-dev <hdjemsmc@gmail.com>
2026-07-09 17:49:56 +0300

    прикручен CORS

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express, {Request, Response} from 'express'
 import {computerRouter} from './routes/computerRoutes'
 import "dotenv/config"
+import cors from 'cors'
 
 const app = express()
 const PORT = process.env.PORT
 
 app.use(express.json())
+app.use(cors())
 app.use('/api/computers', computerRouter)
 
 app.get('/', (req: Request, res: Response) => {
